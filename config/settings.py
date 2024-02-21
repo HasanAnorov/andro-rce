01f250606a9b2f25aa2e0c0849d5bd2beba887e4
EldorKarimov <hacktivist19021@gmail.com>
2024-02-21 14:45:40 +0500

    some changed

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
